aa5b10d6ba1f1236da7401c617df719b2c6fe68b
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2023-12-15 17:15:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+#Rock Paper Scissors Game App
+
 import random
 
+#Intro
 print('Welcome to Rock, Paper, Scissors!')
+#Input choice
 playerChoice = int(input('What do you choose? Type "0" for Rock, "1" for Paper, and "2" for Scissors.\n'))
+#Assign choices to variables
 a, b, c = 'rock', 'paper', 'scissors'
-print(f'You chose {playerChoice})
+print(f'You chose  {playerChoice}')
+#PC choice
 pcChoice = random.randint(0,2)
-print(f'Computer chose {pcChoice})
+print(f'Computer chose {pcChoice}')
+#Compare choices
 if playerChoice == pcChoice:
     print('Draw! Game over. ')
 elif playerChoice == 0 and pcChoice == 2:
